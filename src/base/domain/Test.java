ead4c1944cc5c5eb5d58439f85d98e0ee2e344f8
fuerming <15313482893@163.com>
2021-04-20 12:38:33 +0800

    初始化

---
+++ src/base/domain/Test.java
@@ -0,0 +1,15 @@
+package designPattern.src.base.domain;
+
+import java.io.Serializable;
+
+/**
+ * description:
+ *
+ * @date 2021/4/20 12:28
+ */
+public class Test implements Serializable {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
